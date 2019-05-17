f70746e0715299c92623a5f29d9c3eec8a51ccfe
Rossum, Bart-Jan van <bart-jan.vanrossum@wur.nl>
2019-05-17 15:43:21 +0200

    Modified outlier test for changed function

--- tests/testthat/test-outliers.R
+++ tests/testthat/test-outliers.R
@@ -4,63 +4,64 @@ modelLm <- fitTD(testTD, design = "rowcol", traits = paste0("t", 1:4),
                  engine = "lme4")
 
 test_that("outliersSSA functions properly", {
-  out1 <- outlierSSA(modelLm, trial = "E1", traits = "t1", verbose = FALSE)
+  out1 <- outlierSSA(modelLm, trials = "E1", traits = "t1", verbose = FALSE)
   expect_is(out1, "list")
   expect_length(out1, 2)
-  expect_is(out1$indicator, "data.frame")
-  expect_equal(sum(out1$indicator[["t1"]]), 0)
+  expect_is(out1$indicator, "list")
+  expect_null(out1$indicator[["E1"]][["t1"]])
   expect_null(out1$outliers)
 })
 
 test_that("outliersSSA functions properly for multiple traits", {
-  out14 <- outlierSSA(modelLm, trial = "E1", traits = paste0("t", 1:4),
+  out14 <- outlierSSA(modelLm, trials = "E1", traits = paste0("t", 1:4),
                       verbose = FALSE)
   expect_is(out14, "list")
   expect_length(out14, 2)
-  expect_is(out14$indicator, "data.frame")
-  expect_equal(colnames(out14$indicator), paste0("t", 1:4))
-  expect_equivalent(colSums(out14$indicator), rep(x = 0, times = 4))
+  expect_is(out14$indicator, "list")
+  expect_named(out14$indicator, "E1")
+  expect_named(out14$indicator[["E1"]], paste0("t", 1:4))
   expect_null(out14$outliers)
 })
 
 test_that("option what functions properly", {
-  out1 <- outlierSSA(modelLm, trial = "E1", traits = "t1", what = "random",
+  out1 <- outlierSSA(modelLm, trials = "E1", traits = "t1", what = "random",
                      verbose = FALSE)
   expect_is(out1, "list")
   expect_length(out1, 2)
-  expect_is(out1$indicator, "data.frame")
-  expect_equal(sum(out1$indicator[["t1"]]), 0)
+  expect_is(out1$indicator, "list")
+  expect_null(out1$indicator[["E1"]][["t1"]])
   expect_null(out1$outliers)
 })
 
 test_that("option rLimit funtions properly", {
-  out1 <- outlierSSA(modelLm, trial = "E1", traits = "t1", rLimit = 1,
+  out1 <- outlierSSA(modelLm, trials = "E1", traits = "t1", rLimit = 1,
                      verbose = FALSE)
-  expect_equal(sum(out1$indicator[["t1"]]), 4)
+  expect_length(out1$indicator[["E1"]][["t1"]], 4)
   expect_equal(nrow(out1$outliers), 4)
   expect_equal(out1$outliers$res, c(1.73562640255469, -1.73562640255469,
                                     1.48592104460245, -1.48592104460245))
 })
 
 test_that("option rLimit funtions properly for multiple traits", {
-  out14 <- outlierSSA(modelLm, trial = "E1", traits = paste0("t", 1:4),
+  out14 <- outlierSSA(modelLm, trials = "E1", traits = paste0("t", 1:4),
                       rLimit = 1, verbose = FALSE)
-  expect_equivalent(colSums(out14$indicator), c(4, 6, 2, 2))
+  expect_equivalent(sapply(X = out14$indicator[["E1"]], FUN = length),
+                    c(4, 6, 2, 2))
   expect_equal(nrow(out14$outliers), 14)
 })
 
 test_that("option commonFactors functions properly", {
-  out1 <- outlierSSA(modelLm, trial = "E1", traits = "t1", rLimit = 1,
+  out1 <- outlierSSA(modelLm, trials = "E1", traits = "t1", rLimit = 1,
                      commonFactors = "subBlock", verbose = FALSE)
-  expect_equal(sum(out1$indicator[["t1"]]), 4)
+  expect_length(out1$indicator[["E1"]][["t1"]], 4)
   expect_equal(nrow(out1$outliers), 12)
   expect_equal(sum(out1$outliers$similar), 8)
 })
 
 test_that("option verbose functions properly", {
-  printOut1 <- capture.output(out1 <- outlierSSA(modelLm, trial = "E1",
+  printOut1 <- capture.output(out1 <- outlierSSA(modelLm, trials = "E1",
                                                  traits = "t1", verbose = TRUE))
-  printOut2 <- capture.output(out1 <- outlierSSA(modelLm, trial = "E1",
+  printOut2 <- capture.output(out1 <- outlierSSA(modelLm, trials = "E1",
                                                  traits = "t1", what = "random",
                                                  rLimit = 2, verbose = TRUE))
   expect_equal(printOut1, "No large standardized residuals.")
