bacd24494ca96c3c5e760efd242ed2bccb211d46
Rossum, Bart-Jan van <bart-jan.vanrossum@wur.nl>
2019-12-02 16:23:15 +0100

    More testing

--- tests/testthat/test-fitTDSpATS.R
+++ tests/testthat/test-fitTDSpATS.R
@@ -180,8 +180,9 @@ test_that("Fitting models functions properly when trait contains space", {
   ## Create a trait with a space in its name.
   ## fitTD should be able to handle this.
   #testTD[["E1"]][["t 2"]] <- testTD[["E1"]][["t2"]]
-  expect_silent(modelSp <- fitTD(testTD, design = "rowcol", engine = "SpATS",
-                                 traits = c("t1")))
+  #expect_silent(modelSp <- fitTD(testTD, design = "rowcol", engine = "SpATS",
+  #                               traits = c("t1")))
+  modelSp <- fitTD(testTD, design = "rowcol", traits = "t1")
   expect_SSA(modelSp)
 })
 
